a6acc78394de5a285c5a70016f36b3578c34bd79
kanadOncode-141 <kbbiswas01.hl@gmail.com>
2022-06-19 17:12:26 +0530

    navbar and footer

---
+++ kanad/components/footer.js
@@ -0,0 +1,107 @@
+function footer(){
+    return `
+    <div id="Media">
+    <div id="app">
+        <p>download app</p>
+        <img src="https://www.zee5.com/images/play_store.png?ver=2.51.66" alt="GooglePlay">
+        <img src="https://www.zee5.com/images/app_store.png?ver=2.51.66" alt="AppStore">
+    </div>
+
+    <div id="social">
+        <p>contact with us</p>
+        
+        <a href="https://www.facebook.com/ZEE5/">
+            <img src="../kanad/img/facebook.png" alt="facebook" id="face">
+        </a>
+
+        <a href="https://www.instagram.com/zee5/">
+            <img src="../kanad/img/instagram.png" alt="instagram" id="insta">
+        </a>
+
+        <a href="https://twitter.com/zee5india">
+            <img src="../kanad/img/twitter.png" alt="twitter" id="twit">
+        </a>
+
+        <a href="https://www.youtube.com/channel/UCXOgAl4w-FQero1ERbGHpXQ">
+            <img src="../kanad/img/youtube.png" alt="youtube" id="you">
+        </a>
+
+    </div>
+
+</div>
+
+<div id="fin">
+    <p class="fin2">about us</p>
+    <p>|</p>
+    <p class="fin2">help center</p>
+    <p>|</p>
+    <p class="fin2">privacy policy</p>
+    <p>|</p>
+    <p class="fin2">terms of use</p>
+</div>
+
+<div id="program">
+    <div class="list">
+        <div class="leading">popular tv shows</div>
+        <ul>
+            <li>kumkum bhagya</li>
+            <li>kundali bhagya</li>
+            <li>bhagya lakshmi</li>
+            <li>tujhse hai raabta</li>
+            <li>kyun rishton main katti batti</li>
+        </ul>
+    </div>
+
+    <div class="list">
+        <div class="leading">premium movies</div>
+        <ul>
+            <li>kya meri sonam gupta bewafa hai?</li>
+            <li>helmet</li>
+            <li>200 halla ho</li>
+            <li>14 phere</li>
+            <li>dial 100</li>
+        </ul>
+    </div>
+
+    <div class="list">
+        <div class="leading">popular LIVE TV Channels</div>
+        <ul>
+            <li>aaj tak</li>
+            <li>zee news</li>
+            <li>zee TV HD</li>
+            <li>&TV HD</li>
+            <li>zee marithi HD</li>
+        </ul>
+    </div>
+
+    <div class="list">
+        <div class="leading">popular web series</div>
+        <ul>
+            <li>sunflower</li>
+            <li>jeet ki zid</li>
+            <li>bicchoo ka khel</li>
+            <li>state of siege: 26/11</li>
+            <li>naxalbari</li>
+        </ul>
+    </div>
+
+    <div class="list">
+        <div class="leading">bollywood top celebrities</div>
+        <ul>
+            <li>sunny leone</li>
+            <li>disha patani</li>
+            <li>deepika padukone</li>
+            <li>salman khan</li>
+            <li>nora fatehi</li>
+        </ul>
+    </div> 
+</div>
+
+<div id="last">
+    <div>Best viewed on Google Chrome 80+, Microsoft Edge 81+, Mozilla Firefox 75+, Safari 5.1.5+</div>
+    <div>Copyright © 2022 Zee Entertainment Enterprises Ltd. All rights reserved.</div>
+</div>
+    `;
+}
+
+export default footer;
